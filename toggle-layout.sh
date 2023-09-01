ed345128f1396d44ab962fcfd57abee5866b569e
Artem Smaznov <ArtemSmaznov@gmail.com>
2023-09-01 15:46:17 -0400

    feat(keys): switch layouts

---
+++ toggle-layout.sh
@@ -0,0 +1,13 @@
+#!/usr/bin/env bash
+layout="$1"
+
+if [ ! "$layout" ]; then
+    current_layout=$(hyprctl getoption general:layout | grep str | awk '{print $2}' | tr -d '"')
+    if [ "$current_layout" = "dwindle" ]; then
+        layout="master"
+    else
+        layout="dwindle"
+    fi
+fi
+
+hyprctl keyword general:layout "$layout"
