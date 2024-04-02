772f8541fc42d4956bae4e4e86808f1ea8ae3356
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-04-01 23:59:22 -0400

    fix(toggle-zen): race condition with toggling status bar

--- scripts/toggle-zen.sh
+++ scripts/toggle-zen.sh
@@ -1,23 +1,18 @@
 #!/usr/bin/env bash
+# options ----------------------------------------------------------------------
 default_borders="$1"
 default_corners="$2"
 default_gaps="$3"
 
-# environment variables
-#-------------------------------------------------------------------------------
+# environment variables ________________________________________________________
 [ ! "$XDG_CONFIG_HOME" ] && export XDG_CONFIG_HOME="$HOME/.config"
 
-# variables
-#-------------------------------------------------------------------------------
+# variables ====================================================================
 borders=$(hyprctl getoption general:border_size | grep int | awk '{print $NF}' | tr -d '"')
 corners=$(hyprctl getoption decoration:rounding | grep int | awk '{print $NF}' | tr -d '"')
 gaps=$(hyprctl getoption general:gaps_in | grep type | awk '{print $NF}' | tr -d '"')
 
-# functions
-#-------------------------------------------------------------------------------
-
-# setup
-#-------------------------------------------------------------------------------
+# setup ________________________________________________________________________
 if [[ $borders = 0 && $corners = 0 && $gaps = 0 ]]; then
     bar_action="open"
     border_size="$default_borders"
@@ -30,11 +25,11 @@ else
     gap_size=0
 fi
 
-# execution
-#===============================================================================
-"$XDG_CONFIG_HOME"/hypr/scripts/toggle-status-bar.sh "$bar_action"
+# execution ********************************************************************
 hyprctl --batch "\
     keyword general:border_size $border_size ; \
     keyword decoration:rounding $rounding ; \
     keyword general:gaps_in $gap_size ; \
     keyword general:gaps_out $((gap_size *= 2)) "
+
+"$XDG_CONFIG_HOME"/hypr/scripts/toggle-status-bar.sh "$bar_action"
