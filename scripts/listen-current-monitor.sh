f083abf09457da08836de0dc20c654e07275a8d3
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-07-10 23:40:54 -0400

    fix(scripts): update socket location to XDG_RUNTIME_DIR

--- scripts/listen-current-monitor.sh
+++ scripts/listen-current-monitor.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - |
+socat -u UNIX-CONNECT:"$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - |
   stdbuf -o0 awk -F '>>|,' -e '/^focusedmon>>/ {print $2}'
--- scripts/listen-window-title.sh
+++ scripts/listen-window-title.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 /home/artem/.config/hypr/scripts/get-window-title.sh
-socat -u UNIX-CONNECT:/tmp/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock - |
+socat -u UNIX-CONNECT:"$XDG_RUNTIME_DIR/hypr/$HYPRLAND_INSTANCE_SIGNATURE/.socket2.sock" - |
   stdbuf -o0 awk -F '>>|,' '/^activewindow>>/{print $3}'
