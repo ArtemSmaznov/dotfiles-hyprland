8bfb4b212e5cdfc5dde93e3833a0f4de0ad95a3b
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-11-05 18:51:48 -0500

    refactor(scripts): extrapolate generic logic

--- scripts/bordersctl.sh
+++ scripts/bordersctl.sh
@@ -6,22 +6,26 @@ step="$3"
 option="general:border_size"
 type="int"
 
-current_value=$(hyprctl getoption "$option" | grep "$type" | awk '{print $NF}' | tr -d '"')
+function setValue() {
+    current_value=$(hyprctl getoption "$option" | grep "$type" | awk '{print $NF}' | tr -d '"')
 
-case "$action" in
-reset) value=$default_value ;;
-toggle) [ "$current_value" = 0 ] && value=$default_value || value=0 ;;
-increase) value=$(("$current_value" + "$step")) ;;
-decrease)
-    [ "$current_value" -eq 0 ] && exit 0
-    value=$(("$current_value" - "$step"))
-    ;;
-disable) value=0 ;;
-double) value=$(("$default_value" * 2)) ;;
-*)
-    echo "error: unrecognized option '$action'"
-    exit 1
-    ;;
-esac
+    case "$1" in
+    reset) value=$default_value ;;
+    toggle) [ "$current_value" = 0 ] && value=$default_value || value=0 ;;
+    increase) value=$(("$current_value" + "$step")) ;;
+    decrease)
+        [ "$current_value" -eq 0 ] && exit 0
+        value=$(("$current_value" - "$step"))
+        ;;
+    disable) value=0 ;;
+    double) value=$(("$default_value" * 2)) ;;
+    *)
+        echo "error: unrecognized option '$1'"
+        exit 1
+        ;;
+    esac
+}
+
+setValue "$action"
 
 hyprctl keyword "$option" "$value"
--- scripts/gapsctl.sh
+++ scripts/gapsctl.sh
@@ -7,23 +7,27 @@ option="general:gaps_in"
 option2="general:gaps_out"
 type="custom type"
 
-current_value=$(hyprctl getoption "$option" | grep "$type" | awk '{print $NF}' | tr -d '"')
+function setValue() {
+    current_value=$(hyprctl getoption "$option" | grep "$type" | awk '{print $NF}' | tr -d '"')
 
-case "$action" in
-reset) value=$default_value ;;
-toggle) [ "$current_value" = 0 ] && value=$default_value || value=0 ;;
-increase) value=$(("$current_value" + "$step")) ;;
-decrease)
-    [ "$current_value" -eq 0 ] && exit 0
-    value=$(("$current_value" - "$step"))
-    ;;
-disable) value=0 ;;
-double) value=$(("$default_value" * 2)) ;;
-*)
-    echo "error: unrecognized option '$action'"
-    exit 1
-    ;;
-esac
+    case "$1" in
+    reset) value=$default_value ;;
+    toggle) [ "$current_value" = 0 ] && value=$default_value || value=0 ;;
+    increase) value=$(("$current_value" + "$step")) ;;
+    decrease)
+        [ "$current_value" -eq 0 ] && exit 0
+        value=$(("$current_value" - "$step"))
+        ;;
+    disable) value=0 ;;
+    double) value=$(("$default_value" * 2)) ;;
+    *)
+        echo "error: unrecognized option '$1'"
+        exit 1
+        ;;
+    esac
+}
+
+setValue "$action"
 
 hyprctl keyword "$option" "$value"
 hyprctl keyword "$option2" $((value * 2))
