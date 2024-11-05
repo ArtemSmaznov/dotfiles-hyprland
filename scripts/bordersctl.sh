f3fb38d162f475531213d00164e77e893edef6ea
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-11-04 23:37:14 -0500

    feat(keys): gapsctl

---
+++ scripts/bordersctl.sh
@@ -0,0 +1,24 @@
+#!/usr/bin/env bash
+default_value=$1
+action="$2"
+step="$3"
+
+option="general:border_size"
+type="int"
+
+current_value=$(hyprctl getoption "$option" | grep "$type" | awk '{print $NF}' | tr -d '"')
+
+case "$action" in
+reset) value=$default_value ;;
+toggle) [ "$current_value" = 0 ] && value=$default_value || value=0 ;;
+increase) value=$(("$current_value" + "$step")) ;;
+decrease) value=$(("$current_value" - "$step")) ;;
+disable) value=0 ;;
+double) value=$(("$default_value" * 2)) ;;
+*)
+    echo "error: unrecognized option '$action'"
+    exit 1
+    ;;
+esac
+
+hyprctl keyword "$option" "$value"
