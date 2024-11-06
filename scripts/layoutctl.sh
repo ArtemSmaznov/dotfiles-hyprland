ddeb9340e782b12c937e197a6bc749d609d3e71b
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-11-05 22:46:21 -0500

    feat(keys): move layouts to leader submap
    * add keys for specific layouts

--- scripts/layoutctl.sh
+++ scripts/layoutctl.sh
@@ -1,6 +1,7 @@
 #!/usr/bin/env bash
 layout="$1"
 
+# TODO align with gaps/borders-ctl scripts
 if [ ! "$layout" ]; then
     current_layout=$(hyprctl getoption general:layout | grep str | awk '{print $2}' | tr -d '"')
     if [ "$current_layout" = "dwindle" ]; then
