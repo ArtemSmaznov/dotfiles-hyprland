29c891a16313dd4e1a03af7d3f5aa73327f48aa0
Artem Smaznov <ArtemSmaznov@gmail.com>
2024-12-22 22:02:34 -0500

    feat(hyprctl): add idle call

--- scripts/hyprctl.sh
+++ scripts/hyprctl.sh
@@ -12,9 +12,18 @@ getClientsByClass() {
         jq ".[] | select( .class | contains(\"$1\") )"
 }
 
+getIdleInhibitStatus() {
+    hyprctl clients |
+        grep -e inhibitingIdle |
+        grep 1 &>/dev/null &&
+        echo true ||
+        echo false
+}
+
 # setup ________________________________________________________________________
 
 # execution ********************************************************************
 case "$1" in
 clients) getClientsByClass "$2" ;;
+idle) getIdleInhibitStatus "$2" ;;
 esac
