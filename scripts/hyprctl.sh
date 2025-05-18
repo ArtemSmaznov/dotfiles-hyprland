6c07387201ec2931c4c36af0f197d1e8b987089e
Artem Smaznov <ArtemSmaznov@gmail.com>
2025-05-18 11:47:11 -0400

    feat(scripts): add moveFloatingWindow & unlock to hyprctl.sh

--- scripts/hyprctl.sh
+++ scripts/hyprctl.sh
@@ -1,13 +1,14 @@
 #!/usr/bin/env bash
+
 # functions ~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~
-getClientsByClass() {
+function getClientsByClass() {
     hyprctl clients -j |
         jq ".[] |
             select( .class | contains(\"$1\") )
         "
 }
 
-getIdleInhibitStatus() {
+function getIdleInhibitStatus() {
     hyprctl clients |
         grep -e inhibitingIdle |
         grep 1 &>/dev/null &&
@@ -15,8 +16,42 @@ getIdleInhibitStatus() {
         echo false
 }
 
+function moveFloatingWindow() {
+    window='current'
+    x=84
+    y=61
+    width=1200
+    height=645
+    step=$1
+    direction="$2"
+
+    echo $x,$y
+
+    case "$direction" in
+    l) [ $x -le $step ] && x=0 || x=$(($x - $step)) ;;
+    r) [ $x -le $step ] && x=0 || x=$(($x + $step)) ;;
+    u) [ $y -le $step ] && y=0 || y=$(($y - $step)) ;;
+    d) [ $y -le $step ] && y=0 || y=$(($y + $step)) ;;
+    *) echo "error: invalid direction, $direction" && exit 1 ;;
+    esac
+
+    echo $x,$y
+}
+
+function unlock() {
+    hyprctl \
+        --instance 0 \
+        --batch '''
+            keyword misc:allow_session_lock_restore 1 ;
+            dispatch exec hyprlock ;
+            keyword misc:allow_session_lock_restore 0
+        '''
+}
+
 # execution ********************************************************************
 case "$1" in
 clients) getClientsByClass "$2" ;;
 idle) getIdleInhibitStatus "$2" ;;
+move) moveFloatingWindow $2 "$3" ;;
+unlock) unlock ;;
 esac
