7c3fc9d88386125b62695d58d40e6e4bc5df9006
Artem Smaznov <ArtemSmaznov@gmail.com>
2026-05-17 12:22:41 -0400

    refactor(keys): global RESET_SUBMAP

--- keys.lua
+++ keys.lua
@@ -1,3 +1,10 @@
+RESET_SUBMAP = function()
+  hl.dispatch(hl.dsp.submap("reset"))
+  hl.dispatch(hl.dsp.exec_cmd(HYPR_SCRIPTS .. "/reset-submap.sh"))
+end
+
+--------------------------------------------------------------------------------
+
 require("keys.core")
 require("keys.leader")
 require("keys.mouse")
