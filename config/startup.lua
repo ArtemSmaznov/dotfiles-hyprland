c9e04fde98aa44c9fa6df588f5de5840d05d4c4f
Artem Smaznov <ArtemSmaznov@gmail.com>
2026-05-18 11:54:38 -0400

    fix(startup): open quick-settings

--- config/startup.lua
+++ config/startup.lua
@@ -2,6 +2,6 @@ hl.on("hyprland.start", function()
   hl.exec_cmd("exec uwsm finalize")
   hl.exec_cmd("docker ps") -- HACK
   hl.exec_cmd("paplay \"$HOME/public/audio/windows95-startup.wav\"")
-  hl.exec_cmd("uwsm app -S both -- eww open-many main-bar second-bar " .. EWW_INDICATORS .. EWW_QUICK_SETTINGS)
+  hl.exec_cmd("uwsm app -S both -- eww open-many main-bar second-bar " .. EWW_INDICATORS .. " " .. EWW_QUICK_SETTINGS)
   hl.exec_cmd("hyprpm reload")
 end)
